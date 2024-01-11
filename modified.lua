854c7b13760e377fab31ed9ac5c5fded2bf6311b
rinyur <155040740+rinyur@users.noreply.github.com>
2024-01-11 14:25:20 +0800

    Update modified.lua

--- modified.lua
+++ modified.lua
@@ -144,7 +144,7 @@ local function tryPurchase(uid, gems, item, version, shiny, amount, username, cl
         end
     end)
     repeat task.wait() until signal.Connected == false
-    local boughtPet, boughtMessage = game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+    local boughtPet, boughtMessage = rs.Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
     processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, class, boughtMessage, snipeNormal)
 end
 
