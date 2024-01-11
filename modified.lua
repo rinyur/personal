01fae1af60402f919f41c0c4bdac64d5e0bcc077
rinyur <155040740+rinyur@users.noreply.github.com>
2024-01-11 13:17:41 +0800

    Update modified.lua

--- modified.lua
+++ modified.lua
@@ -141,10 +141,10 @@ local function tryPurchase(uid, gems, item, version, shiny, amount, username, cl
     signal = game:GetService("RunService").Heartbeat:Connect(function()
 	if buytimestamp < workspace:GetServerTimeNow() - Players.LocalPlayer:GetNetworkPing() then
 	    local boughtPet, boughtMessage = game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+	    processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, class, boughtMessage, snipeNormal)
 	    signal:Disconnect()
         end
     end)
-    processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, class, boughtMessage, snipeNormal)
 end
 
 Booths_Broadcast.OnClientEvent:Connect(function(username, message)
